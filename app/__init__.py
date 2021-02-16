b898ce82877404b3160277649f15df5b18f5b53b
Manuel Gaspar <margaspar@gmail.com>
2021-02-16 18:01:07 +0000

    Basic bootstrap theme

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_bootstrap import Bootstrap
 
 WorkScheduler = Flask(__name__)
 WorkScheduler.config.from_object(Config)
@@ -10,5 +11,6 @@ db = SQLAlchemy(WorkScheduler)
 migrate = Migrate(WorkScheduler, db)
 login = LoginManager(WorkScheduler)
 login.login_view = 'login'
+bootstrap = Bootstrap(WorkScheduler)
 
 from app import routes, models
