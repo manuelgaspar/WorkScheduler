ee07f3ab4078350911dc9aaac1a8a7497e4dc9b4
Manuel Gaspar <margaspar@gmail.com>
2021-02-16 17:32:30 +0000

    Added db migrations folder and login/user registration logic

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,13 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 WorkScheduler = Flask(__name__)
 WorkScheduler.config.from_object(Config)
 db = SQLAlchemy(WorkScheduler)
 migrate = Migrate(WorkScheduler, db)
+login = LoginManager(WorkScheduler)
+login.login_view = 'login'
 
 from app import routes, models
